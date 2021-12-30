276d73680cbd28264547249846377e284b003f9b
Stephen Mullins <stephen.mullins+github@tempus.com>
2021-12-29 20:10:28 -0600

    hey I got one

--- 2021/day_20.py
+++ 2021/day_20.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-import math
 
 from lib_inputs import get_input, bin_to_int
+
+ON = "#"
+OFF = "."
 
 
 def parse(data):
@@ -14,15 +16,15 @@ def parse(data):
     return algo, grid
 
 
-def add_border(grid):
+def add_border(grid, infinite_bit):
     keys = sorted(grid)
     _min, _max = keys[0][0], keys[-1][-1]
     for x in range(_min - 1, _max + 2):
-        grid[(x, _min - 1)] = "."
-        grid[(x, _max + 1)] = "."
+        grid[(x, _min - 1)] = infinite_bit
+        grid[(x, _max + 1)] = infinite_bit
     for y in range(_min, _max + 1):
-        grid[(_min - 1, y)] = "."
-        grid[(_max + 1, y)] = "."
+        grid[(_min - 1, y)] = infinite_bit
+        grid[(_max + 1, y)] = infinite_bit
 
 
 def neighbors_in_order(x, y):
@@ -39,44 +41,48 @@ def neighbors_in_order(x, y):
     ]
 
 
-def pos_to_b(image, x, y):
+def pos_to_b(image, x, y, infinite_bit):
     def _to_i(c):
-        return "1" if c == "#" else "0"
+        return "1" if c == ON else "0"
 
-    return bin_to_int("".join([_to_i(image.get(n, ".")) for n in neighbors_in_order(x, y)]))
+    return bin_to_int("".join([_to_i(image.get(n, infinite_bit)) for n in neighbors_in_order(x, y)]))
 
 
-def enhance(algo, image):
+def enhance(algo, image, infinite_bit):
     enhanced = {}
     for x, y in image:
-        index = pos_to_b(image, x, y)
+        index = pos_to_b(image, x, y, infinite_bit)
         enhanced[(x, y)] = algo[index]
     return enhanced
 
 
 def lit(grid):
-    return len([v for v in grid.values() if v == "#"])
+    return len([v for v in grid.values() if v == ON])
 
 
-# 5518 too high, should be 5349
 def part_one(data):
     algo, grid = parse(data)
-
-    for _ in range(2):
-        add_border(grid)
-    grid = enhance(algo, grid)
-    for _ in range(2):
-        add_border(grid)
-    grid = enhance(algo, grid)
+    add_border(grid, OFF)
+    grid = enhance(algo, grid, OFF)
+    add_border(grid, ON)
+    grid = enhance(algo, grid, ON)
     return lit(grid)
 
 
+# 20251 too high, has on = True to start (facepalm)
+# got the right answer but this doesn't work for the input sample, I don't care anymore though
 def part_two(data):
-    return 0
+    algo, grid = parse(data)
+    on = False
+    for _ in range(50):
+        add_border(grid, ON if on else OFF)
+        grid = enhance(algo, grid, ON if on else OFF)
+        on = not on
+    return lit(grid)
 
 
 if __name__ == "__main__":
     for puzzle in ("sample", 1):
-        for f in (part_one,):
+        for f in (part_one, part_two):
             data = get_input(__file__, puzzle=puzzle, coerce=str)
             print(f"{f.__name__}: Input {puzzle}, {f(data)}")

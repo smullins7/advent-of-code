7c3d4cf42f87030bbabc318c469a699ed5274fb6
Stephen Mullins <smullins7@gmail.com>
2022-12-22 09:04:19 -0600

    more wip

--- 2022/day_22.py
+++ 2022/day_22.py
@@ -1,4 +1,5 @@
-from typing import Tuple, Dict
+from dataclasses import dataclass
+from typing import Tuple, Dict, List
 
 from utils.inputs import get_grouped_input
 
@@ -14,6 +15,8 @@ def parse_instructions(line: str):
                 parsed.append(int(current_num))
                 current_num = ""
             parsed.append(c)
+    if current_num:
+        parsed.append(int(current_num))
     return parsed
 
 
@@ -119,10 +122,123 @@ def move(grid: Dict[Tuple[int, int], str], position: Tuple[int, int], facing: st
     return new_position
 
 
+@dataclass
+class Box:
+    min_x: int
+    min_y: int
+    max_x: int
+    max_y: int
+
+
+class Cube:
+    pass
+
+
+def parse_cube(_map: List[str], box_size: int):
+    for y, line in enumerate(_map):
+        for x, c in enumerate(line):
+            if c in (".", "#"):
+                pass
+
+
+def wrap_around(grid, position: Tuple[int, int], facing: str, box_size: int) -> Tuple[Tuple[int, int], str]:
+    if facing == "up":
+        # 1, 2, 3, 6
+        if position[1] == 1:  # 1 -> 5
+            facing = "down"
+        elif position[1] == box_size + 1 and position[0] <= box_size:  # 2 -> 1
+            facing = "down"
+        elif position[1] == box_size + 1:  # 3 -> 1
+            facing = "right"
+        else:  # 6 -> 4
+            facing = "left"
+    elif facing == "left":
+        # 1, 2, 5
+        if position[1] <= box_size:  # 1 -> 3
+            facing = "down"
+        elif position[0] == 1:  # 2 -> 6
+            facing = "up"
+        else:  # 5 -> 3
+            facing = "up"
+    elif facing == "right":
+        # 1, 4, 6
+        if position[1] <= box_size:  # 1 -> 6
+            facing = "left"
+        if box_size < position[1] <= box_size * 2:  # 4 -> 6
+            facing = "down"
+        else:  # 6 -> 1
+            facing = "left"
+    else:  # down
+        # 2, 3, 5, 6
+        if position[0] <= box_size:  # 2 -> 5
+            facing = "up"
+        elif position[0] <= box_size * 2:  # 3 -> 5
+            facing = "right"
+        elif position[0] <= box_size * 3:  # 5 -> 2
+            facing = "up"
+        else:  # 6 -> 2
+            facing = "up"
+
+
+def move_cube(grid, position: Tuple[int, int], facing: str, spaces: int, box_size: int) -> Tuple[int, int]:
+    new_position = position
+    for _ in range(spaces):
+        if facing == "right":
+            # try to move one to the right
+            if grid.get((new_position[0] + 1, new_position[1])) == ".":
+                new_position = new_position[0] + 1, new_position[1]
+            elif grid.get((new_position[0] + 1, new_position[1])) == "#":
+                return new_position
+            else:
+                wrapped = wrap_around(grid, new_position, facing, box_size)
+                if grid[wrapped] == ".":
+                    new_position = wrapped
+                else:
+                    return new_position
+        elif facing == "down":
+            # try to move one down
+            if grid.get((new_position[0], new_position[1] + 1)) == ".":
+                new_position = new_position[0], new_position[1] + 1
+            elif grid.get((new_position[0], new_position[1] + 1)) == "#":
+                return new_position
+            else:
+                wrapped = wrap_around(grid, new_position, facing, box_size)
+                if grid[wrapped] == ".":
+                    new_position = wrapped
+                else:
+                    return new_position
+        elif facing == "left":
+            # try to move one to the left
+            if grid.get((new_position[0] - 1, new_position[1])) == ".":
+                new_position = new_position[0] - 1, new_position[1]
+            elif grid.get((new_position[0] - 1, new_position[1])) == "#":
+                return new_position
+            else:
+                wrapped = wrap_around(grid, new_position, facing, box_size)
+                if grid[wrapped] == ".":
+                    new_position = wrapped
+                else:
+                    return new_position
+        else:  # up
+            # try to move one up
+            if grid.get((new_position[0], new_position[1] - 1)) == ".":
+                new_position = new_position[0], new_position[1] - 1
+            elif grid.get((new_position[0], new_position[1] - 1)) == "#":
+                return new_position
+            else:
+                wrapped = wrap_around(grid, new_position, facing, box_size)
+                if grid[wrapped] == ".":
+                    new_position = wrapped
+                else:
+                    return new_position
+
+    return new_position
+
+
 FACING = ["right", "down", "left", "up"]
 
 
-def part_one(data):
+def part_one(data, **kwargs):
     _map, instructions = data
     grid = parse_map(_map)
     instructions = parse_instructions(instructions[0])
@@ -139,8 +255,23 @@ def part_one(data):
     return 1000 * position[1] + (4 * position[0]) + FACING.index(facing)
 
 
-def part_two(data):
-    return 0
+def part_two(data, is_sample=True):
+    _map, instructions = data
+    box_size = 4 if is_sample else 50
+    # cube = parse_cube(_map, box_size)
+    grid = parse_map(_map)
+    instructions = parse_instructions(instructions[0])
+    position = find_leftmost(grid)
+    facing = "right"
+    for action in instructions:
+        if action == "R":
+            facing = FACING[(FACING.index(facing) + 1) % len(FACING)]
+        elif action == "L":
+            facing = FACING[(FACING.index(facing) - 1) % len(FACING)]
+        else:
+            position = move_cube(grid, position, facing, action, box_size)
+
+    return 1000 * position[1] + (4 * position[0]) + FACING.index(facing)
 
 
 # 200164, 97352

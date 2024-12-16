d2f8f81aa36f811243d188298a349274f05a0d4a
Stephen Mullins <stephen.mullins+github@tempus.com>
2024-12-16 17:36:34 -0600

    randomly went back to 2019 because bored

--- 2019/7/solution.py
+++ 2019/7/solution.py
@@ -6,10 +6,11 @@ PHASES2 = list(range(5,10))
 
 class Program(object):
 
-    def __init__(self, program, stores=None):
+    def __init__(self, program, input_queue: list, output_queue: list):
         self.program = program
         self.pointer = 0
-        self.stores = stores or []
+        self.input_queue = input_queue
+        self.output_queue = output_queue
 
     def next(self, mode="1"):
         v = self.program[self.pointer]
@@ -28,10 +29,13 @@ class Program(object):
         self.pointer = int(pointer)
 
     def input(self):
-        return self.stores.pop(0) if self.stores else input()
+        return self.input_queue.pop()
 
     def output(self, value):
-        self.stores.append(value)
+        self.output_queue.insert(0, value)
+
+    def __str__(self):
+        return f"{self.pointer}, {self.input_queue}, {self.output_queue}"
 
 
 def add(program, modes):
@@ -107,13 +111,15 @@ def parse_instruction(instruction):
 
 def run(program):
     instruction = program.next()
+
     opcode, modes = parse_instruction(instruction)
+
     if opcode == "99":
-        return
+        return False
     OP_CODES[opcode](program, modes)
     if opcode == "04":
-        return
-    run(program)
+        return True
+    return run(program)
 
 
 def part1(inputs):
@@ -129,36 +135,35 @@ def part1(inputs):
     return max_thruster_output
 
 
+def attempt(phase_signals: list[int], instructions) -> int:
+    a_to_b = [phase_signals[1]]
+    b_to_c = [phase_signals[2]]
+    c_to_d = [phase_signals[3]]
+    d_to_e = [phase_signals[4]]
+    e_to_a = [0, phase_signals[0]]
+    amp_a = Program(list(instructions), e_to_a, a_to_b)
+    amp_b = Program(list(instructions), a_to_b, b_to_c)
+    amp_c = Program(list(instructions), b_to_c, c_to_d)
+    amp_d = Program(list(instructions), c_to_d, d_to_e)
+    amp_e = Program(list(instructions), d_to_e, e_to_a)
+    should_run = True
+
+    while should_run:
+        for amp in [amp_a, amp_b, amp_c, amp_d, amp_e]:
+            should_run = run(amp)
+    return int(amp_e.output_queue[0])
+
 def part2(inputs):
     max_thruster_output = 0
     for phases in permutations(PHASES2):
         l = list(phases)
-        amp_a = Program(inputs, stores=[l[0], 0])
-
-        amp_b = Program(list(inputs), stores=[l[1]])
-        amp_c = Program(list(inputs), stores=[l[2]])
-        amp_d = Program(list(inputs), stores=[l[3]])
-        amp_e = Program(list(inputs), stores=[l[4]])
-        while True:
-            run(amp_a)
-            amp_b.stores.insert(0, amp_a.stores.pop())
-            run(amp_b)
-            amp_c.stores.insert(0, amp_b.stores.pop())
-            run(amp_c)
-            amp_d.stores.insert(0, amp_c.stores.pop())
-            run(amp_d)
-            amp_e.stores.insert(0, amp_d.stores.pop())
-            run(amp_e)
-            if amp_e.program[amp_e.pointer] == "99":
-                break
-            amp_a.stores.insert(0, amp_e.stores.pop())
-        max_thruster_output = max(int(amp_e.stores[0]), max_thruster_output)
+        max_thruster_output = max(attempt(l, inputs), max_thruster_output)
 
     return max_thruster_output
 
 
 if __name__ == "__main__":
-    inputs = [line.strip() for line in open("./example.txt")][0].split(",")
+    inputs = [line.strip() for line in open("./input.txt")][0].split(",")
     #print(part1(inputs))
     print(part2(inputs))
 
